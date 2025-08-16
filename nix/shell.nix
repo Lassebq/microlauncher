08c6d0a053e5eb00f67e68b7df03fdbf7ca22b52
NotAShelf <raf@notashelf.dev>
2025-08-16 10:15:28 +0300

    nix: initial packaging & tooling
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a6964381ed5656cb546c88ecaeeb45a415441

---
+++ nix/shell.nix
@@ -0,0 +1,25 @@
+{
+  mkShell,
+  gdb,
+  gtk4,
+  curl,
+  openssl,
+  libuuid,
+  json_c,
+  libzip,
+}:
+mkShell {
+  name = "C";
+
+  packages = [gdb]
+
+  nativeBuildInputs = [meson ninja pkg-config];
+  buildInputs = [
+    gtk4
+    curl
+    openssl
+    libuuid
+    json_c
+    libzip
+  ];
+}
