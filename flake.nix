b595ccd725006fb49040c8d05b0ff8ff875664fe
NotAShelf <raf@notashelf.dev>
2025-08-16 10:29:55 +0300

    nix: add `microlauncher` output; add `hydraJobs`
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a6964db0100982e722d1f7bfdb446c9a13617

--- flake.nix
+++ flake.nix
@@ -1,17 +1,24 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
     systems = ["x86_64-linux" "aarch64-linux"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
     pkgsForEach = nixpkgs.legacyPackages;
   in {
     packages = forEachSystem (system: {
-      default = pkgsForEach.${system}.callPackage ./nix/package.nix {};
+      microlauncher = pkgsForEach.${system}.callPackage ./nix/package.nix {};
+      default = self.packages.${system}.microlauncher;
     });
 
     devShells = forEachSystem (system: {
       default = pkgsForEach.${system}.callPackage ./nix/shell.nix {};
     });
+
+    hydraJobs = self.packages;
   };
 }
